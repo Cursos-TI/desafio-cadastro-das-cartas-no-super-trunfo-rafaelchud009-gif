155bdaeda9fb9615a625f356f2a5984e8a2b4995
chud <rafaelchud009@gmail.com>
2025-08-21 19:32:09 +0000

    *** #273Desenvolvido e testado por Ronald ***

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -46,25 +46,25 @@ int main() {
 
     printf("\n\n=================== CARTA 2 =====================\n");
 
-      printf("Digite o estado (sigla): ");
+      printf("Digite o estado (sigla): "); //exemoli "SP", "RJ"
         scanf("%2s", Estado);
     
-    printf("digite o numero da carta: ");
+    printf("digite o numero da carta: "); // exemplo: '01' , '02'
           scanf("%s", carta);
     
     printf("Digite o nome da cidade: ");
           scanf("%19s", cidade);
     
-    printf("Digite o numero de habitantes: ");  
+    printf("Digite o numero de habitantes: ");   // exemplo: 1000000, 5000000
          scanf("%d", &habitantes);
     
-    printf("Digite a area em km2:");
+    printf("Digite a area em km2:");    // exemplo: 1000 km2, 2000 km2
          scanf("%lf", &area);
     
-    printf("Digite o PIB (em milhões) : ");
+    printf("Digite o PIB (em milhões) : "); // exemplo: 50 , 100
         scanf("%f", &pib);
     
-    printf("Digite os citypoints: ");
+    printf("Digite os citypoints: "); // exemplo: 10, 20
          scanf("%i", &citypoints);
 
     printf("\n\n=================== CARTA JOGADA =====================\n");
